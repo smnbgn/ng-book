26373f505bc7e075d1c022f9960c1a295e7c6193
Simon Bogina <simon@bogina.si>
2016-02-24 15:09:42 +0100

    JS in separate file

---
+++ 15/js/app.js
@@ -0,0 +1,10 @@
+function MyController($scope) {
+  $scope.clock = new Date();
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+  setInterval(function() {
+    $scope.$apply(updateClock);
+  }, 1000);
+  updateClock();
+};
