1f890eecd9766bb6754b825fb59e4eb129bf2d07
Simon Bogina <simon@bogina.si>
2016-03-05 22:20:00 +0100

    Function filter

--- 40/js/app.js
+++ 40/js/app.js
@@ -1,1 +1,6 @@
 var app = angular.module('myApp', []);
+
+app.controller('DemoController', function($scope, $filter){
+  $scope.isCapitalized = function(str)
+    { return str[0] == str[0].toUpperCase(); }
+});
