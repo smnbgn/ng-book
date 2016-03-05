22e5e35cf47f8b6291f186f4a5e60bf8c51d8f93
Simon Bogina <simon@bogina.si>
2016-03-06 00:10:15 +0100

    Custom filters

--- 40/js/app.js
+++ 40/js/app.js
@@ -1,6 +1,14 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['myApp.filters']);
 
 app.controller('DemoController', function($scope, $filter){
   $scope.isCapitalized = function(str)
     { return str[0] == str[0].toUpperCase(); }
+});
+
+angular.module('myApp.filters', [])
+.filter('capitalize', function() {
+  return function(input) {
+    if (input)
+      return input[0].toUpperCase() + input.slice(1);
+  }
 });
