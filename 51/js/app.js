322ee9268cdc23dbb3f4231f47ea3509c701a57c
Simon Bogina <simon@bogina.si>
2016-03-09 06:48:26 +0100

    Form validations

---
+++ 51/js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp', []);
+
+app.directive('ensureUnique', function($http) {
+  return {
+    require: 'ngModel',
+    link: function(scope, ele, attrs, c) {
+      scope.$watch(attrs.ngModel, function(n) {
+        if (!n) return;
+        $http({
+          method: 'POST',
+          url: '/api/check/' + attrs.ensureUnique,
+          data: {'field': attrs.ensureUnique}
+        }).success(function(data){
+          c.$setValidity('unique', data.isUnique);
+        }).error(function(data){
+          c.$setValidity('unique', false)
+        });
+      });
+    }
+  }
+});
